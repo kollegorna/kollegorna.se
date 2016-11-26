8ce12554a9a1fc4b716ff228996b7628fcbe4117
Eduardo Nunes <ed@eduardonunes.me>
2016-11-26 15:28:44 +0000

    Finish first draft of project page

--- source/assets/javascripts/app/app.js
+++ source/assets/javascripts/app/app.js
@@ -4,3 +4,18 @@ $('.feed').packery({
   layoutMode: 'masonry',
   gutter: 20
 });
+
+$('.case__content').on('click', '.expand', function() {
+  $('.case__facts').slideToggle();
+  $('.additional').slideToggle();
+
+  var text = $(this).text();
+
+  if(text === "More detail") {
+    $(this).text("Less detail");
+    $(this).addClass('less');
+  } else {
+    $(this).text("More detail");
+    $(this).removeClass('less');
+  }
+});
