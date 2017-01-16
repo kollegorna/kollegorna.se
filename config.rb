1a698df0d2cca8b11cb5217a70218105e4c1b938
Eduardo Nunes <ed@eduardonunes.me>
2017-01-16 12:39:23 +0000

    Add production fonts

--- config.rb
+++ config.rb
@@ -46,7 +46,9 @@ activate :livereload
 
 # Minimize css/js and fix assets for Build
 configure :build do
-  activate :autoprefixer
+  activate :autoprefixer do |config|
+    config.ignore = ['fonts/535951/']
+  end
   activate :gzip
   activate :minify_css
   activate :minify_javascript
