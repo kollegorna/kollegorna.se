0066ae6a35fc3e53b2d2cd5172d008bf12c6f61b
Osvaldas Valutis <osvaldas.v@gmail.com>
2018-04-11 14:40:40 +0300

    Switch from Bower to NPM

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require packery/dist/packery.pkgd.min.js
 //= require imagesloaded/imagesloaded.pkgd.min.js
-//= require disqusLoader.js/jquery.disqusloader.js
+//  require disqusLoader.js/jquery.disqusloader.js
 //= require lazyLoadGoogleMaps.js/jquery.lazyLoadGoogleMaps.js
 //= require fitvids/dist/fitvids.min.js
 //= require svg4everybody/dist/svg4everybody
